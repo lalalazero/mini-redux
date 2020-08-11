3af07b8a6a0525c8baee26a77eac082796190654
lalalazero <zzzero520@hotmail.com>
2020-08-11 16:14:14 +0800

    encapsulate createStore function

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-var state = {
-    count: 1
-};
-var listeners = [];
-function subscribe(listener) {
-    listeners.push(listener);
-}
-function changeCount(count) {
-    state.count = count;
-    for (var i = 0; i < listeners.length; i++) {
-        listeners[i]();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
     }
-}
-subscribe(function () {
-    console.log(state.count);
+    return t;
+};
+var createStore = function (initState) {
+    var state = initState;
+    var listeners = [];
+    function subscribe(listener) {
+        listeners.push(listener);
+    }
+    function notify() {
+        for (var i = 0; i < listeners.length; i++) {
+            listeners[i]();
+        }
+    }
+    function changeState(newState) {
+        state = newState;
+        notify();
+    }
+    function getState() {
+        return state;
+    }
+    return {
+        subscribe: subscribe,
+        changeState: changeState,
+        getState: getState
+    };
+};
+var initState = {
+    counter: {
+        count: 0
+    },
+    info: {
+        name: '',
+        description: ''
+    }
+};
+var store = createStore(initState);
+store.subscribe(function () {
+    var state = store.getState();
+    console.log(state.info.name + ": " + state.info.description);
 });
-changeCount(1);
-changeCount(2);
-changeCount(3);
+store.subscribe(function () {
+    var state = store.getState();
+    console.log(state.counter.count);
+});
+store.changeState(__assign({}, store.getState(), { info: {
+        name: 'jack',
+        description: 'is a boy'
+    } }));
+store.changeState(__assign({}, store.getState(), { counter: {
+        count: 2
+    } }));
+store.changeState(__assign({}, store.getState(), { counter: {
+        count: 'whatever but not a number'
+    } }));
--- index.ts
+++ index.ts
@@ -1,24 +1,76 @@
-let state = {
-    count: 1
-}
 
-let listeners: Function[] = []
 
-function subscribe(listener: Function) {
-    listeners.push(listener)
-}
+const createStore = function(initState: any){
+    let state = initState
+    let listeners: Function[] = []
 
-function changeCount(count: number) {
-    state.count = count
-    for(let i = 0; i < listeners.length; i++){
-        listeners[i]()
+    function subscribe(listener: Function){
+        listeners.push(listener)
+    }
+
+    function notify(){
+        for(let i = 0; i < listeners.length; i++){
+            listeners[i]()
+        }
+    }
+
+    function changeState(newState: any){
+        state = newState 
+        notify()
+    }
+
+    function getState(){
+        return state
+    }
+
+    return {
+        subscribe,
+        changeState,
+        getState
     }
 }
 
-subscribe(() => {
-    console.log(state.count)
+let initState = {
+    counter: {
+        count: 0
+    },
+    info: {
+        name: '',
+        description: ''
+    }
+}
+
+let store = createStore(initState)
+
+store.subscribe(() => {
+    let state = store.getState()
+    console.log(`${state.info.name}: ${state.info.description}`)
 })
 
-changeCount(1)
-changeCount(2)
-changeCount(3)
+store.subscribe(() => {
+    let state = store.getState()
+    console.log(state.counter.count)
+})
+
+store.changeState({
+    ...store.getState(),
+    info: {
+        name: 'jack',
+        description: 'is a boy'
+    }
+})
+
+
+store.changeState({
+    ...store.getState(),
+    counter: {
+        count: 2
+    }
+})
+
+store.changeState({
+    ...store.getState(),
+    counter: {
+        count: 'whatever but not a number'
+    }
+})
