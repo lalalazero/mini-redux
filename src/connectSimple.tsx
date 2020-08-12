2d5d3a62a4e3dcea083403592440e163bad155c3
lalalazero <zzzero520@hotmail.com>
2020-08-12 17:29:00 +0800

    connectSimple.tsx

---
+++ src/connectSimple.tsx
@@ -0,0 +1,29 @@
+import React , { Component, ComponentType } from 'react'
+import { createStoreSimple } from './createSimple'
+
+const store = createStoreSimple({})
+
+export default function connect(WrapComponent: ComponentType<any>){
+    return class Connected extends Component {
+        constructor(props: {}){
+            super(props)
+            this.state = store.getState()
+        }
+        
+        unsub(){}
+
+        componentDidMount(){
+            this.unsub = store.subscribe(() => {
+                this.setState(store.getState())
+            })
+        }
+
+        componentWillUnmount(){
+            this.unsub()
+        }
+
+        render(){
+            return <WrapComponent  {...this.state} {...this.props} />
+        }
+    }
+}
