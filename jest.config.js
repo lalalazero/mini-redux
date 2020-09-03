45c50513501eb0c6dad39865bfd4e87ebc5dcf90
lalalazero <zzzero520@hotmail.com>
2020-09-03 08:59:22 +0800

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    setupFiles: ['./setup.js']
+    setupFiles: ['./setup.js'],
+    testPathIgnorePatterns: [
+        '<rootDir>/esm',
+        '<rootDir>/cjs'
+    ],
+    testMatch: ["src/**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"]
 }
